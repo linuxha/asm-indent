1df8a418720da99e58473f858abe6e00d0e06229
Neil Cherry <linuxha@linuxha.com>
2025-11-10 20:29:22 -0500

    Update this to correct the function used for each op code

--- asm-indent.py
+++ asm-indent.py
@@ -39,19 +39,25 @@ def func(s):
     asmOps(s)
 #
 
+def func1(s):
+    print(";* Func1")
+    asmOps(s)
+#
+
 def lAsmOps(s):
     pass
 #
 
-#
+# FCC, FCB, RMB can all start without a LABEL
 def asmOps(s):
     s  = s.strip()
+    #print(";* asmOps: <{0}>".format(s))
     sp = s.split(None, 3)
-    #if(len(sp[0]) < 6):
-    #    sp[0] = sp[0] + '\t'
-    #
     l  = len(sp)
-    if(l == 4):
+
+    if(line[0] == ' '):
+        print("\t{0}\t;*".format(s))
+    elif(l == 4):
         if(len(sp[1]) < 4):
             sp[1] = sp[1] + ' '
         #
@@ -70,23 +76,33 @@ def asmOps(s):
     else:
         # If 2
         #   LDA  #1
-        print("1?:{0}".format(s))
+        #   SPC  1
+        #   ORG $xxxx
+        #   RMB  nnn
+        print("\t{0}\t\t;* asmOps?".format(s))
     #
 #
 
-# 	RTS	*
-# 	RTS	* Comment
-# Oops
-#       ASLB            ;* 
+#        RTS
+#        RTS	*
+#        RTS	* Comment
+#        ASLB            ;* 
 #STR1    ASLB BUFFER     ;* EMPTY BIT TO CARRY XXXXXXXX
-
+#        LDAA #2 SET FOR WRITE
+#        STAA 0,X
+#        LDAA #$FF SET FOR BINAIRY WRITE
+#        STAA 59,X
+#
 def func2(s):
     s  = s.strip()
     sp = s.split(None, 1)
     l  = len(sp)
 
+    ###
+    ### @FIXME: Not handling STAA/LDAA (or LDAB, etc) properly
+    ###
     if(l == 2):
-        print("\t{0}\t\t;* {1}".format( sp[0], sp[1]))
+        print("\t{0}\t\t;* {1} (l == 2)".format( sp[0], sp[1]))
     elif(l == 1):
         print("\t{0}\t\t;* ".format( sp[0]))
     else:
@@ -137,6 +153,7 @@ def func4(s):
     s = s.strip()
     sp = s.split(None, 2)
     l  = len(sp)
+
     if(l == 3):
         if(len(sp[0]) < 4):
             sp[0] = sp[0] + ' '
@@ -166,6 +183,7 @@ def func5(s):
     s = s.strip()
     sp = s.split(None, 1)
     l  = len(sp)
+
     if(l == 2):
         print("{0}\t\t;* {1}".format( sp[0], sp[1]))
     elif(l == 1):
@@ -177,10 +195,12 @@ def func5(s):
 
 # <LABEL> ASLA VALUE <Comment...>
 # <LABEL> ASL A      <Comment...>
+#
 def func6(s):
-    s = s.strip()
+    s  = s.strip()
     sp = s.split(None, 1)
     l  = len(sp)
+
     if(l == 2):
         print("{0}\t\t;* {1}".format( sp[0], sp[1]))
     elif(l == 1):
@@ -240,142 +260,145 @@ filename = sys.argv[1];
 # LABEL LSRA		*
 # LABEL ADDA #$30
 # LABEL ADDA #$30	*
-OP = {'RMB': asmOps,
-      'EQU': asmOps,
-      'FDB': asmOps,
-      'FCB': asmOps,
-        'FCC':  func,
-        'OPT':  func,
-        'PAG':  func,
-        'NAM':  func,
-        'NOOPT':func,
-        'SPC':  func,
-        'LIST': func,
-        'TITLE':func,
-        'PAGE': func,
-        'ORG':  func4,
-        'END':  func2,
-        'TTL':  func,
-      'x': asmOps
+OP = {
+    'END':  func2,
+    'EQU': asmOps,
+    'FCB': asmOps,
+    'FCC':  func,
+    'FDB': asmOps,
+    'LIST': func,
+    'NAM':  func,
+    'NOOPT':func,
+    'OPT':  func,
+    'ORG':  func4,
+    'PAG':  func,
+    'PAGE': func,
+    'RMB': asmOps,
+    'SPC':  func,
+    'TITLE':func,
+    'TTL':  func,
+    'x': asmOps
 }
 
 # No labels
 # mnemonics
-NM = {  'RMB':  func,
-        'FDB':  func,
-        'FCB':  func,
-        'EQU':  func,
-        'FCC':  func,
-        'OPT':  func,
-        'PAG':  func,
-        'NAM':  func,
-        'NOOPT':func,
-        'SPC':  func,
-        'LIST': func,
-        'TITLE':func,
-        'PAGE': func,
-        'ORG':  func4,
-        'END':  func2,
-        'TTL':  func,
-      'ABA':  func2,
-      'ABX':  func2,
-      'ADC':  func5,
-      'ADCA': func2,
-      'ADCB': func2,
-      'ADD':  func5,
-      'ADDA': func2,
-      'ADDB': func2,
-      'AND':  func5,
-      'ANDA': func2,
-      'ANDB': func2,
-      'ASL':  func5,
-      'ASLA': func2,
-      'ASLB': func2,
-      'ASR':  func6,
-      'ASRA': func2,
-      'ASRB': func2,
-      'BIT':  func5,
-      'BITA': func2,
-      'BITB': func2,
-      'CBA':  func2,
-      'CLC':  func2,
-      'CLI':  func2,
-      'CLR':  func6,
-      'CLRA': func2,
-      'CLRB': func2,
-      'CLV':  func2,
-      'CMP':  func5,
-      'CMPA': func2,
-      'CMPB': func2,
-      'COM':  func6,
-      'COMA': func2,
-      'COMB': func2,
-      'DAA':  func2,
-      'DEC':  func6,
-      'DECA': func2,
-      'DECB': func2,
-      'DES':  func2,
-      'DEX':  func2,
-      'EOR':  func5,
-      'EORA': func2,
-      'EORB': func2,
-      'INC':  func6,
-      'INCA': func2,
-      'INCB': func2,
-      'INS':  func2,
-      'INX':  func2,
-      'LDA':  func5,
-      'LDAA': func2,
-      'LDAB': func2,
-      'LSR':  func6,
-      'LSRA': func2,
-      'LSRB': func2,
-      'NEG':  func6,
-      'NEGA': func2,
-      'NEGB': func2,
-      'NOP':  func2,
-      'ORA':  func5,
-      'ORAA': func2,
-      'ORAB': func2,
-      'PSH':  func6,
-      'PSHA': func2,
-      'PSHB': func2,
-      'PUL':  func6,
-      'PULA': func2,
-      'PULB': func2,
-      'ROL':  func6,
-      'ROLA': func2,
-      'ROLB': func2,
-      'ROR':  func6,
-      'RORA': func2,
-      'RORB': func2,
-      'RTI':  func2,
-      'RTS':  func2,
-      'SBA':  func2,
-      'SBC':  func5,
-      'SBCA': func2,
-      'SBCB': func2,
-      'SEC':  func2,
-      'SEI':  func2,
-      'SEV':  func2,
-      'STA':  func5,
-      'STAA': func2,
-      'STAB': func2,
-      'SUB':  func5,
-      'SUBA': func2,
-      'SUBB': func2,
-      'SWI':  func2,
-      'TAB':  func2,
-      'TAP':  func2,
-      'TBA':  func2,
-      'TPA':  func2,
-      'TST':  func6,
-      'TSTA': func2,
-      'TSTB': func2,
-      'TSX':  func2,
-      'TXS':  func2,
-      'WAI':  func2
+NM = {
+    'ABA':  func2,
+    'ABX':  func2,
+    'ADC':  func5,
+    'ADCA': func2,
+    'ADCB': func2,
+    'ADD':  func5,
+    'ADDA': func2,
+    'ADDB': func2,
+    'AND':  func5,
+    'ANDA': func2,
+    'ANDB': func2,
+    'ASL':  func5,
+    'ASLA': func2,
+    'ASLB': func2,
+    'ASR':  func6,
+    'ASRA': func2,
+    'ASRB': func2,
+    'BIT':  func5,
+    'BITA': func2,
+    'BITB': func2,
+    'CBA':  func2,
+    'CLC':  func2,
+    'CLI':  func2,
+    'CLR':  func6,
+    'CLRA': func2,
+    'CLRB': func2,
+    'CLV':  func2,
+    'CMP':  func1,
+    'CMPA': func1,
+    'CMPB': func2,
+    'COM':  func6,
+    'COMA': func2,
+    'COMB': func2,
+    'DAA':  func2,
+    'DEC':  func6,
+    'DECA': func2,
+    'DECB': func2,
+    'DES':  func2,
+    'DEX':  func2,
+    'END':  func2,
+    'EOR':  func5,
+    'EORA': func2,
+    'EORB': func2,
+    'EQU':  func,
+    'FCB':  func,
+    'FCC':  func,
+    'FDB':  func,
+    'INC':  func6,
+    'INCA': func2,
+    'INCB': func2,
+    'INS':  func2,
+    'INX':  func2,
+    'LDA':  func5,
+    'LDAA': func2,
+    'LDAB': func2,
+    'LIST': func,
+    'LSR':  func6,
+    'LSRA': func2,
+    'LSRB': func2,
+    'NAM':  func,
+    'NEG':  func6,
+    'NEGA': func2,
+    'NEGB': func2,
+    'NOOPT':func,
+    'NOP':  func2,
+    'OPT':  func,
+    'ORA':  func5,
+    'ORAA': func2,
+    'ORAB': func2,
+    'ORG':  func4,
+    'PAG':  func,
+    'PAGE': func,
+    'PSH':  func6,
+    'PSHA': func2,
+    'PSHB': func2,
+    'PUL':  func6,
+    'PULA': func2,
+    'PULB': func2,
+    'RMB':  func,
+    'ROL':  func6,
+    'ROLA': func2,
+    'ROLB': func2,
+    'ROR':  func6,
+    'RORA': func2,
+    'RORB': func2,
+    'RTI':  func2,
+    'RTS':  func2,
+    'SBA':  func2,
+    'SBC':  func5,
+    'SBCA': func2,
+    'SBCB': func2,
+    'SEC':  func2,
+    'SEI':  func2,
+    'SEV':  func2,
+    'SPC':  func,
+    'STA':  func5,
+    'STAA': func2,
+    'STAB': func2,
+    'SUB':  func5,
+    'SUBA': func2,
+    'SUBB': func2,
+    'SWI':  func2,
+    'TAB':  func2,
+    'TAP':  func2,
+    'TBA':  func2,
+    'TITLE':func,
+    'TPA':  func2,
+    'TST':  func6,
+    'TSTA': func2,
+    'TSTB': func2,
+    'TSX':  func2,
+    'TTL':  func,
+    'TXS':  func2,
+    'WAI':  func2
 }
+
 # NM[idx](str)
 try:
     with open(filename) as f:
@@ -393,7 +416,8 @@ try:
                 print(';*[ Fini ]**********************************************************************')
                 break
             #
-            
+
+            # Blank lines
             if line.isspace() or line == '\n':
                 print(line)
                 continue
@@ -405,24 +429,33 @@ try:
                 continue
             #
 
+            # If the line starts with a * convert to ;* line...
+            if line.startswith('*'):
+                comments(line)
+                #print(";{0}".format(line))
+                continue
+            #
+
             # The difference is that if line is an empty string, line[:1]
             # will evaluate to an empty string while line[0] will
             # raise an IndexError
             #
-            #myRe  = re.compile(r"^\s+|^\s+\*")
-            myRe  = re.compile(r"^\s*\*|^\s*\n")
+            #yRe  = re.compile(r"^\s+|^\s+\*")
+            #yRe  = re.compile(r"^\s*\*|^\s*\n")
+            myRe  = re.compile(r"^[\t ].*")
             match = re.search(myRe, line)
             if match:
+                #print("### '" + line + "' ###")
+                instructions(line)
                 # Handle blank and comment lines
                 # Handle blank and lines that start with comments
-                comments(line)
+                #comments(line)
             elif(line[:1].isalpha()):
                 # Handle labels (and equates)
                 # Label OP VALUE Comment...
                 labels(line)
             else:
-                #print("### '" + line + "' ###")
-                instructions(line)
+                print(";* ??? {0}: {1}".format(ln, line))
             #
         #
     #
@@ -433,52 +466,37 @@ except FileNotFoundError:
 
 # =[ Fini ]=====================================================================
 """
-      'PSHB': func2,
-      'ASLA': func2,
-      'ASLB': func2,
-      'LSRA': func2,
-      'LSRB': func2,
-      'TAB':  func2,
-      'ABA':  func2,
-      'PULB': func2,
-      'PSHA': func2,
-      'PULA': func2,
-      'INX':  func2,
-      'DECA': func2,
-      'DECB': func2,
-      'DEX':  func2,
-      'INCA': func2,
-      'INCB': func2,
-      'INX':  func2,
-      'INS':  func2,
-      'CLRA': func2,
-      'CLRB': func2,
-      'TSX':  func2,
-      'TXS':  func2,
+Problem with NAM, OPT, SPC, RMB (with no label),LDAA, STAA, etc.
 
-      'ADC':  func5,
-      'ADD':  func5,
-      'AND':  func5,
-      'ASL':  func6,
-      'ASR':  func6,
-      'BIT':  func5,
-      'CLR':  func6,
-      'CMP':  func5,
-      'COM':  func6,
-      'DEC':  func6,
-      'EOR':  func5,
-      'INC':  func6,
-      'LDA':  func5,
-      'LSL':  func6, ???
-      'LSR':  func6,
-      'NEG':  func6,
-      'ORA':  func5,
-      'PSH':  func6,
-      'PUL':  func6,
-      'ROL':  func6,
-      'ROR':  func6,
-      'SBC':  func5,
-      'STA':  func5,
-      'SUB':  func5,
-      'TST':  func6,
+A line is:
+     <OP> [<values>] <comment>                  | @FIXME lines (user will need to fix)
+     <OP> [<values>]                            | include,nam
+     <OP>                                       |
+    <LABEL> <OP> <VALUE> <Comment...>           | LABEL LDA A ,X Comment 
+    <LABEL> <OP> <Comment...>                   | LABEL RTS Comment
+     <OP> <VALUE> <Comment...>                  |       STA A ,X Comment
+     <OP> <Comment...>                          |       CLR A Comment
+     <OP>                                       |       NOP
+
+Things like STA A or LDA B need to be converted to STAA and LDAB
+
+- A Label always starts in Column 0
+- There is always whitespace before an <OP>
+- An <OP> can have {0,1}<VALUE>
+- A <VALUE> can never have whitespace in it
+- A <Comment...> always starts with whitespace
+
+INC        ;* ERRTOT
+should be
+        INC  ERRTOT
+
+Also
+
+        CMPA            ;* #$D    |  CMPA #$D
+        BEQ  NXTLQ      ;*        |  BEQ NXTLQ
+        INX             ;*        |  INX
+        CMPA            ;* #':    |  CMPA #':
+        BEQ  CONTL      ;*        |  BEQ  CONTL
+        LDAA            ;* #4     |  LDAA #4
+
 """
